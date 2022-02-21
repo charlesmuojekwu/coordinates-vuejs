755cb7c0cce6bb8ed9a2766bdbc31722a41dc378
Charles Muojekwu <charlesmuojekwu@gmail.com>
2022-02-21 10:38:03 +0100

    update with bootstrap and buttons to toggle between current and last coordinated

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,21 +1,31 @@
 <template>
     <div>
         <div style="margin:0 auto; align:center">
+            
+            <hr>
             <h1>My coordinates</h1>
             <p>{{ MyCoordinates.lat }} Latitude, {{ MyCoordinates.lng }} Longitude</p>
-
-            <h1>Map coordinates</h1>
+            <hr>
+            <button class="btn-sm btn-success" style="margin: 0 5px" @click="ShowInMap()">Show My coordinates</button>
+            <button class="btn-sm  btn-primary">Show My Last coordinates</button>
+            <hr>
+           
+            <h1>Map Showing {{ status }} coordinates</h1>
             <p>{{ MapCoordinates.lat }} Latitude, {{ MapCoordinates.lng }} Longitude</p>
-       
-            
+
             <GmapMap
-                :center="MyCoordinates"
+                :center="MapShowCoordinates"
                 :zoom="zoom"
                 style="width:80%; height:360px; margin:auto"
                 ref="mapRef"
                 @dragend="handleDrag"
             >
             </GmapMap>
+            <p style="font-size:10px">Map showing your last coordinates</p>
+
+            <h1>Last Set coordinates</h1>
+            <p>{{ LastCoordinates.lat }} Latitude, {{ LastCoordinates.lng }} Longitude</p>
+
         </div>
     </div>
 </template>
@@ -33,39 +43,70 @@ export default {
               lng: 0
           },
 
+          LastCoordinates: {
+              lat: 0,
+              lng: 0
+          },
+
+          MapShowCoordinates: {
+              lat: 0,
+              lng: 0
+          },
+
+          status:'pending'
+
       }
     },
 
-    created() {
-        
+    methods : {
 
-        // check for center in local storage
-        if(localStorage.center) {
-            this.MyCoordinates = JSON.parse(localStorage.center);
-        }else{
+        ShowInMap(){
 
-            this.$getLocation({})
+            // set map coordinates to my coordinate
+            this.MapShowCoordinates = this.MyCoordinates;
+
+            // set showing to current
+            this.status = 'Current'
+            
+            // save as last coordinates
+            let center = {
+                lat: this.MyCoordinates.lat,
+                lng: this.MyCoordinates.lng
+            };
+
+            let zoom = this.map.getZoom()
+
+            localStorage.center = JSON.stringify(center);
+            localStorage.zoom = zoom;
+        },
+
+
+        /// current coordinates methods
+        getLocation() {
+             this.$getLocation({})
             .then((coordinates) => {
                 this.MyCoordinates = coordinates
             })
             .catch((error) => alert(error)); 
-
-        }
-
-         // check for zoom in local storage
-        if(localStorage.zoom) {
-            this.zoom = JSON.parse(localStorage.zoom);
-        }
-
-    },
-
-    /// runs after vue component loaded
-    mounted() {
-        this.$refs.mapRef.$mapPromise.then(map =>this.map = map);
-    },
+        },
 
 
-    methods : {
+        //last coordinate method
+        getLastLocation(){
+
+             // check for center in local storage
+            if(localStorage.center) {
+                this.MapShowCoordinates = JSON.parse(localStorage.center); //load in map
+                this.LastCoordinates = JSON.parse(localStorage.center); // show below the map
+            }
+
+            // check for zoom in local storage
+            if(localStorage.zoom) {
+                this.zoom = JSON.parse(localStorage.zoom);
+            }
+        },
+
+
         handleDrag() {
             let center = {
                 lat: this.map.getCenter().lat(),
@@ -78,6 +119,24 @@ export default {
             localStorage.zoom = zoom;
         }
     },
+
+    created() {
+        
+        /// load current coordinaates on top 
+        this.getLocation();
+       
+       /// load LAst coordinaates on MAP 
+        this. getLastLocation();
+
+    },
+
+    /// runs after vue component loaded
+    mounted() {
+        this.$refs.mapRef.$mapPromise.then(map =>this.map = map);
+    },
+
+
+
 
     computed: {
         MapCoordinates() {
